047005b1803ff6170be0afed2b284388512c9cf1
예윤 <yunye@yeyun-ui-MacBookAir.local>
2023-01-31 15:21:43 +0900

    [ADD] 초기 환경 세팅

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const routes = require("./api/routes");
+
+const createApp = () => {
+  const app = express();
+
+  app.use(cors());
+  app.use(morgan("dev"));
+  app.use(express.json());
+  app.use(routes);
+
+  app.get("/ping", (req, res) => {
+    res.json({ message: "pong" });
+  });
+  return app;
+};
+
+module.exports = { createApp };
