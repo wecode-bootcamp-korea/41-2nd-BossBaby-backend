49d9b5aa923f2eadeda00ae47a29c56e003bc1cf
김동규 <pythakyu@gmail.com>
2023-02-09 18:13:31 +0900

    [MODIFY] mypageController 불필요한 에러 핸들링 삭제 및 불필요한 property 수정

--- api/controllers/mypageController.js
+++ api/controllers/mypageController.js
@@ -2,31 +2,19 @@ const mypageService = require('../services/mypageService');
 const { catchAsync } = require('../middlewares/error');
 
 const getSellerInfo = catchAsync(async (req, res) => {
-  const userId = req.userId.userId.id;
-
-  if (!userId) {
-    return res.status(400).json({ message: 'INPUT_ERROR' });
-  }
-  const result = await mypageService.getSellerInfo(userId);
+  const { userId } = req.userId;
+  const result = await mypageService.getSellerInfo(userId.id);
   return res.status(200).json({ userInfo: result });
 });
 
 const getSellerReview = catchAsync(async (req, res) => {
   const userId = req.userId.userId.id;
-
-  if (!userId) {
-    return res.status(400).json({ message: 'INPUT_ERROR' });
-  }
-
   const result = await mypageService.getSellerReview(userId);
   return res.status(200).json({ sellerReviews: result });
 });
 
 const getLikesList = catchAsync(async (req, res) => {
   const userId = req.userId.userId.id;
-  if (!userId) {
-    return res.status(400).json({ message: 'INPUT_ERROR' });
-  }
 
   const result = await mypageService.getLikesList(userId);
   return res.status(200).json({ products: result });
@@ -34,20 +22,12 @@ const getLikesList = catchAsync(async (req, res) => {
 
 const getSellingProducts = catchAsync(async (req, res) => {
   const userId = req.userId.userId.id;
-  if (!userId) {
-    return res.status(400).json({ message: 'INPUT_ERROR' });
-  }
-
   const result = await mypageService.getSellingProducts(userId);
   return res.status(200).json({ products: result });
 });
 
 const getOrderProducts = catchAsync(async (req, res) => {
   const userId = req.userId.userId.id;
-  if (!userId) {
-    return res.status(400).json({ message: 'INPUT_ERROR' });
-  }
-
   const result = await mypageService.getOrderProducts(userId);
   return res.status(200).json({ products: result });
 });
